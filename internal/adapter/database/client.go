bc6e6040a227936a7f300d9758ececd71a79ff70
Illia Aleksiienko <illia.aleksiienko.dev@gmail.com>
2025-12-17 13:52:23 +0100

    deleted client db interface

--- internal/adapter/database/client.go
+++ internal/adapter/database/client.go
@@ -6,15 +6,6 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-// type Client interface {
-// 	Begin(context.Context) (pgx.Tx, error)
-// 	Exec(context.Context, string, ...any) (pgconn.CommandTag, error)
-// 	QueryRow(context.Context, string, ...any) pgx.Row
-// 	Query(context.Context, string, ...any) (pgx.Rows, error)
-// 	Ping(context.Context) error
-// 	Close()
-// }
-
 type WithTransaction interface {
 	WithTransaction(ctx context.Context, fn func(pgx.Tx) error) error
 }
